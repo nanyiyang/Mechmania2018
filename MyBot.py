dd85e07cebf25d801e6d8b6ee16e3fa9f3f210ee
joshuacheng <32106994+joshuacheng@users.noreply.github.com>
2018-09-21 23:16:13 -0500

    Crude speed prioritization

--- MyBot.py
+++ MyBot.py
@@ -19,6 +19,36 @@ def get_winning_stance(stance):
     elif stance == "Scissors":
         return "Rock"
 
+def pick_dest_for_speed(game, me):
+    destination_node = -1
+    monsters = game.nearest_monsters(me.location, 1)
+
+
+    for monster in monsters:
+        if monster.death_effects.speed > 0:
+            paths = game.shortest_paths(me.location, monster.location)
+            destination_node = paths[random.randint(0, len(paths) - 1)][0]
+
+    return destination_node
+
+# checks if our destination will kill us
+def will_dest_kill(game, me, destination_node):
+    if (!has_monster(destination_node)):
+
+
+def random_move(game, me):
+    # get all living monsters closest to me
+    monsters = game.nearest_monsters(me.location, 1)
+
+    # choose a monster to move to at random
+    monster_to_move_to = monsters[random.randint(0, len(monsters)-1)]
+
+    # get the set of shortest paths to that monster
+    paths = game.shortest_paths(me.location, monster_to_move_to.location)
+    destination_node = paths[random.randint(0, len(paths)-1)][0]
+
+    return destination_node
+
 # main player script logic
 # DO NOT CHANGE BELOW ----------------------------
 for line in fileinput.input():
@@ -33,18 +63,22 @@ for line in fileinput.input():
 
     me = game.get_self()
 
+    h_weight = 0.2
+    r_weight = 0.2
+    sc_weight = 0.2
+    p_weight = 0.2
+    s_weight = 0.2
+
     if me.location == me.destination: # check if we have moved this turn
-        # get all living monsters closest to me
-        monsters = game.nearest_monsters(me.location, 1)
+        destination_node = pick_dest_for_speed(game, me)
 
-        # choose a monster to move to at random
-        monster_to_move_to = monsters[random.randint(0, len(monsters)-1)]
+        if destination_node == -1:
+            destination_node = random_move(game, me)
 
-        # get the set of shortest paths to that monster
-        paths = game.shortest_paths(me.location, monster_to_move_to.location)
-        destination_node = paths[random.randint(0, len(paths)-1)][0]
+
     else:
         destination_node = me.destination
+
 
     if game.has_monster(me.location):
         # if there's a monster at my location, choose the stance that damages that monster
