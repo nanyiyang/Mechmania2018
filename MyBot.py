79cc60d5eec52573578dade5a8a24af0dfc5e63f
nanyiyang <ryan.nanyi@gmail.com>
2018-09-22 02:41:18 -0500

    trying to make health and speed farming functions

--- MyBot.py
+++ MyBot.py
@@ -19,8 +19,63 @@ def get_winning_stance(stance):
     elif stance == "Scissors":
         return "Rock"
 
+def farm_speed(me):
+    monsters = game.nearest_monsters(me.location, 1)
+    for monster in monsters:
+        if monster.death_effects.speed > 0:
+            paths = game.shortest_paths(me.location, monster.location)
+            to_move_to = paths[random.randint(0, len(paths) - 1)][0]
+    return to_move_to
+
+def farm_health(me):
+    monsters = game.nearest_monsters(me.location, 1)
+    for monster in monsters:
+        if monster.death_effects.health > 0:
+            paths = game.shortest_paths(me.location, monster.location)
+            to_move_to = paths[random.randint(0, len(paths) - 1)][0]
+    return to_move_to
+
+
 # main player script logic
 # DO NOT CHANGE BELOW ----------------------------
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.insert(0,item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
+q = Queue()
+
+def should_i_farm(me):
+    if me.paper + me.scissors + me.rock >= 8:
+        return False
+    if me.health < 40:
+        return False
+    return True
+
+def should_i_farm_speed(me):
+    if me.speed > 0:
+        return False
+    return True
+
+def should_i_farm_health(me):
+    if not should_i_farm_speed:
+        return True
+
 for line in fileinput.input():
     if first_line:
         game = game_API.Game(json.loads(line))
@@ -32,17 +87,27 @@ for line in fileinput.input():
     # code in this block will be executed each turn of the game
 
     me = game.get_self()
-    #test
+    enemy = game.get_opponent();
 
     if me.location == me.destination: # check if we have moved this turn
         # get all living monsters closest to me
-        monsters = game.nearest_monsters(me.location, 1)
+        if should_i_farm_speed == True:
+            to_move_to = farm_speed(me)
 
+        if should_i_farm_health == True:
+            to_move_to = farm_health(me)
+
+        monsters = game.nearest_monsters(me.location, 1)
         # choose a monster to move to at random
-        monster_to_move_to = monsters[random.randint(0, len(monsters)-1)]
+        to_move_to = monsters[0]
+
+
+
+        if not should_i_farm(me):
+            to_move_to = enemy
 
         # get the set of shortest paths to that monster
-        paths = game.shortest_paths(me.location, monster_to_move_to.location)
+        paths = game.shortest_paths(me.location, to_move_to.location)
         destination_node = paths[random.randint(0, len(paths)-1)][0]
     else:
         destination_node = me.destination
@@ -53,6 +118,17 @@ for line in fileinput.input():
     else:
         # otherwise, pick a random stance
         chosen_stance = stances[random.randint(0, 2)]
+
+    q.push(enemy.stance)
+
+    if q.size() > 3:
+        q.pop()
+
+    if enemy.location == me.location and enemy.stance in ["Rock", "Scissors", "Paper"]:
+        chosen_stance = get_winning_stance(most_common(q.items))
+
+    if chosen_stance not in ["Rock", "Scissors", "Paper"]:
+        chosen_stance = "Rock"
 
     # submit your decision for the turn (This function should be called exactly once per turn)
     game.submit_decision(destination_node, chosen_stance)
